f877adf030ca19233a76b8a23239d79d87fb39a7
danielfenex <68931226+danielfenex@users.noreply.github.com>
2024-10-09 23:56:01 -0700

    Added RegEx Notes + Functions

--- example.hs
+++ example.hs
@@ -1,3 +1,4 @@
+-- import qualified Control.Monad.RWS as them
 -- Prepositional Formulas
 
 data Form = T | F |Neg Form | Cnj Form Form | Dsj Form Form deriving Show
@@ -81,3 +82,126 @@ add = \n -> case n of
             Z -> \m -> m
             S n' -> \m -> add n' (S m)
 -- see photo3
+
+-- go back and make all s into S
+
+-- [add S S z] s s s z
+-- \n -> \m -> case n of [z -> \m; S n' -> add n' [S m]] [s s z] s s s z 
+-- \m -> case (s s z) of [ z -> \m; S n' -> add n' [S m]] (s s s z)
+-- case ( s s z) of [ z -> s s s z; S n' -> add n' [s s s s z]]
+-- add [s z] [s s s s z]
+
+
+-- ----------------------------------------------------
+-- -- 3. Another recursive type: lists/strings
+
+data IntList = Empty | NonEmpty Int IntList deriving Show
+
+myList :: IntList
+myList = NonEmpty 5 (NonEmpty 7 (NonEmpty 2 Empty))
+
+-- Two versions of the 'total' function from the handout
+total :: IntList -> Int
+total = \l -> case l of
+              Empty -> 0
+              NonEmpty x rest -> x + total rest
+
+-- built-in haskell lists
+-- [] empty
+-- : non-empty 'cons' non-empty
+otherTotal :: [Int] -> Int
+otherTotal = \l -> case l of
+                   [] -> 0
+                   x:rest -> x + otherTotal rest
+
+-- total( 5: ...)
+-- ....
+
+-- can write [ 5, 7, 2] instead of (5 : (7 : (2 : [])))
+
+
+
+-- ----------------------------------------------------
+-- 3.1 Polymorphism
+
+data Shape = Rock | Paper | Scissors deriving (Eq, Show)
+
+myIntList :: [Int]
+myIntList = [5,7,2]
+
+myBoolList :: [Bool]
+myBoolList = [True, False, True, True]
+
+myShapeList :: [Shape]
+myShapeList = [Rock, Scissors, Scissors, Rock, Paper]
+
+-- see the many different types of char lists
+-- :t "abc"
+-- -> [ Char]
+
+-- containsRock :: [Shape] -> Bool
+-- containsRock = case dan of 
+--                 [] ->  False
+--                 dan : rest -> case dan of {Rock -> True; Paper -> containsRock rest ; Scissors -> containsRock rest;}
+
+
+size = \l -> case l of
+                [] -> Z
+                x : rest -> S (size rest)
+
+-- size :: [a] -> Numb
+
+-- see photo for polymorphic type
+
+
+
+
+
+
+-- Suppose we wanted to write a function for checking whether something 
+-- is an element of a list, which would work with various types, like this:
+--      elementOf Rock [Rock,Paper] ==>* True
+--      elementOf 3 [1,2,4,5] ==>* False
+
+-- We might start with something like the following. 
+-- elementOf :: a -> ([a] -> Bool)
+-- elementOf = \x -> \l -> case l of
+--                         [] -> False
+--                         y : rest -> case (??? x ??? y ???) of
+--                                     ??? -> True
+--                                     ??? -> elementOf x rest
+
+-- Here is one solution that works: we can ``outsource'' the equality-checking to the 
+-- caller of the elementOf function.
+-- elementOf :: (a -> a -> Bool) -> (a -> ([a] -> Bool))
+-- elementOf = \isEqual -> \x -> \l -> case l of
+--                                     [] -> False
+--                                     y : rest -> case (isEqual x y) of
+--                                                 True -> True
+--                                                 False -> elementOf isEqual x rest
+
+-- Haskell gives us a more convenient way of doing something which is 
+-- equivalent (under the hood) to the outsourcing solution.
+
+elementOf :: (Eq a) => (a -> ([a] -> Bool))
+elementOf = \x -> \l -> case l of
+                        [] -> False
+                        y : rest -> case (x == y) of
+                                    True -> True
+                                    False -> elementOf x rest
+
+-- notice the '(Eq a) =>' in the type definition
+-- this only works with types that have Eq defined in them.
+
+-- Can we make Shape part of the Eq type class
+-- Yes, by making the type definition contain
+-- 'deriving (Eq, Show)' instead of ...
+
+
+
+
+
+
+
+
+
